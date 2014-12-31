b2a6fbb3d53dab0759464fa3bf281a13b69e771e
Brian O'Keefe <brian@bokstuff.com>
2014-12-30 23:50:04 -0500

    Spec helper cleanups

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,15 +4,15 @@ require 'webmock/rspec'
 WebMock.disable_net_connect!
 
 RSpec.configure do |config|
-  config.before(:each) do
+  config.before :each do
     stub_request(:get, 'https://www.select-a-spot.com/bart/').
-      to_return(:status => 200, :body => File.read('fixtures/mock_home_page.html'), :headers => {})
+      to_return :status => 200, :body => File.read('fixtures/mock_home_page.html')
 
     stub_request(:post, 'https://www.select-a-spot.com/bart/users/login/').
-      to_return(:status => 200, :body => 'it worked', :headers => {})
+      to_return :status => 200, :body => 'it worked'
 
     stub_request(:get, 'https://www.select-a-spot.com/bart/waiting_lists/').
-      to_return(:status => 200, :body => File.read('fixtures/mock_waiting_list_page.html'), :headers => {})
+      to_return :status => 200, :body => File.read('fixtures/mock_waiting_list_page.html')
   end
 end
 
