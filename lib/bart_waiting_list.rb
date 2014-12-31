8fa1093e47ca598a0e3350ceab5e75f0841377b7
Brian O'Keefe <brian@bokstuff.com>
2014-12-31 00:19:06 -0500

    Move stations to yaml file

--- lib/bart_waiting_list.rb
+++ lib/bart_waiting_list.rb
@@ -1,4 +1,5 @@
 require 'mechanize'
+require 'yaml'
 
 HOME_URL = 'https://www.select-a-spot.com/bart/'
 WAITING_LIST_URL = 'https://www.select-a-spot.com/bart/waiting_lists/'
@@ -51,39 +52,12 @@ class BartWaitingList
   end
 
   def get_station_name(station)
-    {
-      :ashby => 'Ashby Station',
-      :bay_fair => 'Bay Fair Station',
-      :castro_valley => 'Castro Valley Station',
-      :coliseum_oakland_airport => 'Coliseum/Oakland Airport Station',
-      :colma => 'Colma Station',
-      :concord => 'Concord Station',
-      :daly_city => 'Daly City Station',
-      :dublin_pleasanton => 'Dublin/Pleasanton Station',
-      :el_cerrito_del_norte => 'El Cerrito del Norte Station',
-      :el_cerrito_plaza => 'El Cerrito Plaza Station',
-      :fremont => 'Fremont Station',
-      :fruitvale => 'Fruitvale Station',
-      :hayward => 'Hayward Station',
-      :lafayette => 'Lafayette Station',
-      :lake_merritt => 'Lake Merritt Station',
-      :macarthur => 'MacArthur Station',
-      :millbrae => 'Millbrae Station',
-      :north_berkeley => 'North Berkeley Station',
-      :north_concord_martinez => 'North Concord/Martinez Station',
-      :orinda => 'Orinda Station',
-      :pittsburg_bay_point => 'Pittsburg/Bay Point Station',
-      :pleasant_hill_contra_costa_centre => 'Pleasant Hill/Contra Costa Centre Station',
-      :richmond => 'Richmond Station',
-      :rockridge => 'Rockridge Station',
-      :san_bruno => 'San Bruno Station',
-      :san_leandro => 'San Leandro Station',
-      :south_hayward => 'South Hayward Station',
-      :south_san_francisco => 'South San Francisco Station',
-      :union_city => 'Union City Station',
-      :walnut_creek => 'Walnut Creek Station',
-      :west_dublin_dublin => 'West Dublin Station - Dublin',
-      :west_dublin_pleasanton => 'West Dublin Station - Pleasanton'
-    }[station]
+    station_names = {}
+
+    YAML.load_file('stations.yaml').each do |key, value|
+      station_names[key.to_sym] = value
+    end
+
+    return station_names[station]
   end
 end
